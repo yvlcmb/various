bf1c4a8339e75de42a5374a9c7c639dc48a5088b
slingload <yvlcmb@protonmail.com>
2023-04-07 12:05:25 -0500

    Update mobility.rkt

--- mobility.rkt
+++ mobility.rkt
@@ -1,13 +1,41 @@
 #lang racket
-(define (main)
-  (define stryker (hash "weight" 36320 "axles" 4))
-  (= (factor-weight stryker) 0.314622))
+(require rackunit)
 
-(define (factor-weight vehicle)
+(define (test-factors)
+  (define stryker (hash "weight" 36320  ;lbs
+                        "clearance" 21  ;inches
+                        "axles" 4
+                        "wheel#" 8
+                        "tire-width" 22  ;inches
+                        "hydraluic" #t
+                        "tire-diameter" 45  ;inches
+                        "tire#" 8))
+  (check-equal? (factor-weight stryker) 0.314622)
+  (check-equal? (factor-wheel-load stryker )4 27/50)
+  (check-equal? (factor-grouser stryker) 1)
+  (check-equal? (factor-tire stryker) 25/8)
+  (check-equal? (factor-transmission stryker) 1 1/20)
+  (check-equal? (factor-engine stryker) 1)
+  (check-equal? (factor-contact-pressure stryker) 9 17/99)
+  (check-equal? (factor-clearance stryker) 2 1/10)
+  (check-equal? (calculate-mobility-index stryker) 3.531569664)
+  )
+
+(define (calculate-mobility-index vehicle)
   #|
   :param vehicle: hash table of vehicle properties
-  :return: a number reflecting the vehicle's weight factor
+  :return: a number reflecting the vehicle's mobility index
   |#
+  (*
+   (-
+    (+
+     (/ (* (factor-contact-pressure vehicle) (factor-weight vehicle))
+        (* (factor-tire vehicle) (factor-grouser vehicle)))
+     (factor-wheel-load vehicle))
+   (factor-clearance vehicle))
+   (* (factor-engine vehicle) (factor-transmission vehicle))))
+
+(define (factor-weight vehicle)
   (local ((define lbs/axle (/ (hash-ref vehicle "weight") (hash-ref vehicle "axles")))
           (define kips (/ (hash-ref vehicle "weight") 1000)))
   (let ([mods (cond
@@ -17,4 +45,30 @@
                 [else `(0.278 -3.115)])])
   (+ (* (first mods) (/ kips (hash-ref vehicle "axles")) (last mods))))))
 
-(display (main))
+(define (factor-tire vehicle)
+  (/ 100 (+ 10 (hash-ref vehicle "tire-width"))))
+
+(define (factor-grouser vehicle)
+  (if (hash-has-key? vehicle "chains") 1 1/20 1))
+
+(define (factor-wheel-load vehicle)
+  (let ([kips (/ (hash-ref vehicle "weight") 1000)])
+    (/ kips (hash-ref vehicle "wheel#"))))
+
+(define (factor-clearance vehicle)
+  (/ (hash-ref vehicle "clearance") 10))
+
+(define (factor-transmission vehicle)
+  (if (hash-has-key? vehicle "hydraulic") 1 1 1/20))
+
+(define (factor-engine vehicle)
+  (let ([hp/ton (/ (hash-ref vehicle "weight") 2000)])
+    (if (<= hp/ton 10)  1 1/20 1)))  
+
+(define (factor-contact-pressure vehicle)
+  (/ (hash-ref vehicle "weight")
+     (* (hash-ref vehicle "tire-width")
+        (hash-ref vehicle "tire#") 
+        (/ (hash-ref vehicle "tire-diameter") 2))))
+  
+(test-factors)
