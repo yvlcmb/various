9f6832211deb7961bfc39de632a1b2766ddb560d
yvlcmb <yvlcmb@protonmail.com>
2025-03-19 13:52:02 -0500

    Update innovation.fsx
    add sample cards

--- innovation.fsx
+++ innovation.fsx
@@ -25,5 +25,12 @@ module Innovation =
        color = color
        icons = icons
        dogma_icon = dogma_icon }
-  
+
+  /// sample cards
   let domestication = createCard "domestication" 1 "yellow" (None, Some "castle", Some "castle", Some "castle") "castle"
+  let metalWorking = { domestication with name = "Metal Working"; color = "red" } 
+  let theWheel = { name = "The Wheel"; 
+                   age = 1;
+                   color = "green"; 
+                   icons = (None, Some "castle", Some "castle", Some "castle");
+                   dogma_icon = "castle" }
